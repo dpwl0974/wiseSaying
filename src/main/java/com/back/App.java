cdcf337ff8a32d7854c69fe448753d762a690072
dpwl0974 <dpwl0974@gmail.com>
2025-08-07 11:31:24 +0900

    refactor: ArrayList 변환

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,19 +1,21 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //핵심 로직 클래스
 public class App {
     // ✅ run() 및 다른 함수 사용위해 인스턴스 변수로 변경 (지역 변수 X)
     private Scanner sc = new Scanner(System.in);
-    private int lastNo = 0; //등록 번호
-    private int lastIndex = 0; //배열 인덱스
-    private WiseSaying[] wiseSayings = new WiseSaying[100]; //크기 100 배열
+    private int lastId = 0; //등록 번호
+    //private int lastIndex = 0; //배열 인덱스 / ArrayList에는 필요없는 부분
+    //private WiseSaying[] wiseSayings = new WiseSaying[100]; //크기 100 배열
+    //배열의 단점을 보완하기 위한 ArrayList 추가 / List 안에 ArrayList, LinkedList 등 포함
+    private List<WiseSaying> wiseSayings = new ArrayList<>(); //자동 형변환
 
-    //배열의 단점을 보완하기 위한 ArrayList 추가 예정
 
-
-    //외부에서 꼭 써야하는 메소드 => public 그 외 기본 private
+    //외부에서 꼭 써야하는 메서드 => public 그 외 기본 private
     public void run() {  //main 무조건 static ⭕️ 그 외 static ❌
         System.out.println("== 명언 앱 ==");
 
@@ -30,10 +32,10 @@ public class App {
             } else if (command.startsWith("삭제")) { //삭제로 시작하는
                 actionDelete(command);
 
-            } else if (command.startsWith("수정")){
+            } else if (command.startsWith("수정")) {
                 actionModify(command);
 
-            } else if(command.equals("종료")) {
+            } else if (command.equals("종료")) {
                 break;
             }
         }
@@ -54,11 +56,12 @@ public class App {
 
     //등록 (데이터 처리 담당)
     private WiseSaying write(String saying, String author) {
-        lastNo++;
+        lastId++;
 
-        WiseSaying wiseSaying = new WiseSaying(lastNo, saying, author); //인스턴스 생성
+        WiseSaying wiseSaying = new WiseSaying(lastId, saying, author); //인스턴스 생성
 
-        wiseSayings[lastIndex++] = wiseSaying;
+        wiseSayings.add(wiseSaying);
+
 
         return wiseSaying; //actionWrite()에서 값 받기 위해 return
     }
@@ -68,17 +71,18 @@ public class App {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
-        WiseSaying[] wiseSayings = findListDesc();
+        List<WiseSaying> wiseSayings = findListDesc();
 
-        for(WiseSaying wiseSaying : wiseSayings) { //배열 훑기
+        for (WiseSaying wiseSaying : wiseSayings) { //배열 훑기
             // 변수 사용 시 코드 길어짐 -> 포맷팅 효율적
             System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getSaying(), wiseSaying.getAuthor()));
         }
     }
 
     //목록 (데이터 처리)
-    private WiseSaying[] findListDesc() {
-        //내림차순으로 저장할 배열
+    private List<WiseSaying> findListDesc() {
+
+        /*//내림차순으로 저장할 배열
         WiseSaying[] resultList = new WiseSaying[lastIndex];
         int resultListIndex = 0;
 
@@ -86,8 +90,8 @@ public class App {
         for(int i = lastIndex -1; i >= 0; i--) {
             resultList[resultListIndex] = wiseSayings[i];
             resultListIndex++;
-        }
-        return resultList;
+        }*/
+        return wiseSayings.reversed(); //역순
     }
 
 
@@ -96,7 +100,7 @@ public class App {
 
         String[] commandBits = command.split("=");
 
-        if(commandBits.length < 2) {
+        if (commandBits.length < 2) {
             System.out.println("번호를 입력해주세요. ");
             return; //종료
         }
@@ -105,10 +109,9 @@ public class App {
         int id = Integer.parseInt(idStr); //문자열 -> 숫자
 
         boolean result = delete(id);
-        if(result) {
+        if (result) {
             System.out.println("%d번 명언이 삭제되었습니다.".formatted(id));
-        }
-        else {
+        } else {
             System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
         }
     }
@@ -119,15 +122,17 @@ public class App {
         int deleteTargetIndex = findIndexById(id); // 삭제하고 싶은 명언이 저장된 위치
 
 
-        if(deleteTargetIndex == -1) {
+        if (deleteTargetIndex == -1) {
             return false; //종료
         }
 
-        for(int i = deleteTargetIndex; i < lastIndex; i++) {
+        //ArrayList에는 "삭제" 존재하므로 필요 없는 코드
+        /*for(int i = deleteTargetIndex; i < lastIndex; i++)
             wiseSayings[i] = wiseSayings[i + 1];
-        }
+        }*/
 
-        lastIndex--;
+        wiseSayings.remove(deleteTargetIndex);
+
         return true;
     }
 
@@ -135,7 +140,7 @@ public class App {
     private void actionModify(String command) {
         String[] commandBits = command.split("="); // '=' 기준 분할
 
-        if(commandBits.length < 2) {
+        if (commandBits.length < 2) {
             System.out.println("번호를 입력해주세요. ");
             return; //종료
         }
@@ -145,12 +150,13 @@ public class App {
 
         int modifyTargetIndex = findIndexById(id);
 
-        if(modifyTargetIndex == -1) {
+
+        if (modifyTargetIndex == -1) {
             System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
             return;
         }
 
-        WiseSaying modifyTargetWiseSaying = wiseSayings[modifyTargetIndex];
+        WiseSaying modifyTargetWiseSaying = wiseSayings.get(modifyTargetIndex);
 
         System.out.println("명언(기존) : %s".formatted(modifyTargetWiseSaying.getSaying()));
         System.out.print("명언 : ");
@@ -171,11 +177,12 @@ public class App {
 
     //id 찾기 (삭제, 수정)
     private int findIndexById(int id) {
-        for (int i = 0; i < lastIndex; i++) {
-            if (wiseSayings[i].getId() == id) {
-                return i; //명언 번호 반환
+        for (int i = 0; i < wiseSayings.size(); i++) {
+            if (wiseSayings.get(i).getId() == id) {
+                return i;
             }
         }
         return -1;
     }
 }
+
